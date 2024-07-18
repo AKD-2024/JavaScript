262fb9ba9ed0466c46abafdc0eeead3cdb756510
Anjali <anjali.dubey@comprotechnologies.com>
2024-07-18 09:58:57 +0530

    JS: Training.

--- 15_iife.js
+++ 15_iife.js
@@ -9,3 +9,14 @@
     console.log(`DB ${num}, Connected!`);
 })(2); //Output: DB 2, Connected
 
+// Hoisting
+// Built in behaviour of JS in which the function or variable declaration are moved to the top of their scope so that they can be used before declaration
+
+a = 2;
+var a;
+console.log(a); // 2
+
+// or
+
+console.log(b); // undefined as only variable declaration is moved to the top of their scope
+var b = 2;

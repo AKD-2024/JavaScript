54b08aecf407726d6163e60a909600f2b1ba74ad
Anjali <anjali.dubey@comprotechnologies.com>
2024-07-19 17:10:31 +0530

    JS: Training.

--- 19_object_loops.js
+++ 19_object_loops.js
@@ -9,3 +9,10 @@ const myObj = {
 for (const key in myObj) {
     console.log(`${key} shortcut is for ${myObj[key]}`);
 }
+
+/*
+js shortcut is for javascript
+cpp shortcut is for C++
+rb shortcut is for ruby
+swift shortcut is for swift by apple
+*/ 

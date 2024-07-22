e1218d1f56979f821a79c3492b0cb0eefbac5335
Anjali <anjali.dubey@comprotechnologies.com>
2024-07-22 16:39:00 +0530

    JS: Training.

--- 20_arrayAdv_loops.js
+++ 20_arrayAdv_loops.js
@@ -53,9 +53,7 @@ console.log(totalPrice); //9997
 // chaining - allows us to use multiple maps, or maps and filter together, result of first is passed to next and so on ...
 
 const nums = [1, 2, 3, 4, 5];
-
 const numbers = nums.map((num) => num * 10).map((num) => num + 1).filter( (num) => num>30);
 
 // [1, 2, 3, 4, 5] is passed to first map and [10, 20, 30, 40, 50] is passed from first map to second map [11, 21, 31, 41, 51] is passed from second map to filter and now filter will give result as [31, 41, 51];
-
 console.log(numbers); // [31, 41, 51]

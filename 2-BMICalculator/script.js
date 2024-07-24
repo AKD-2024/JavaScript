8477060148ffb2611bca943cd18070afda099506
Anjali <anjali.dubey@comprotechnologies.com>
2024-07-24 12:36:37 +0530

    JS Project-2: BMI Calculator

---
+++ 2-BMICalculator/script.js
@@ -0,0 +1,25 @@
+const form = document.querySelector("form");
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const height = parseInt(document.querySelector("#height").value);
+    const weight = parseInt(document.querySelector("#weight").value);
+    const result = document.querySelector("#results");
+    const message = document.querySelector("#message");
+
+    if(height === "" || height < 0 || isNaN(height) ) {
+        result.innerHTML = `Please enter a valid height ${height}`
+    }
+
+    if (weight === "" || weight < 0 || isNaN(weight)) {
+      result.innerHTML = `Please enter a valid weight ${weight}`;
+    }
+
+    const bmi = (weight / ((height*height)/10000)).toFixed(2);
+    result.innerHTML=`<span>${bmi}</span>`
+
+    if(bmi < 18.6) message.innerHTML = `Under Weight`
+    else if(bmi >= 18.6 && bmi<=24.9) message.innerHTML = `Normal Range`;
+    else if(bmi>24.9) message.innerHTML = `Over Weight`;
+});
